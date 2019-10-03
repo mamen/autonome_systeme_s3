fc7607b8d124e2187cca59a0dcdf86d806255f83
mamen <mamen@mamen.at>
2019-10-03 12:18:36 +0200

    Initial commit

---
+++ unterlagen/TurtlebotCode/initializeTurtleBlobs.sh
@@ -0,0 +1,3 @@
+source ~/catkin_ws/devel/setup.bash 
+rosrun pixy_node pixy_node &
+roslaunch turtlebot3_bringup turtlebot3_robot.launch
---
+++ unterlagen/TurtlebotCode/initializeTurtleCam.sh
@@ -0,0 +1,3 @@
+source ~/catkin_ws/devel/setup.bash 
+rosrun beginner_camera pixy_cam &
+roslaunch turtlebot3_bringup turtlebot3_robot.launch
