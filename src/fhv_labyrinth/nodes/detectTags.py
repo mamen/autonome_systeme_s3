cdc0db85eecade3dfd4928ddaa1b46b3eff3fc27
mamen <mamen@mamen.at>
2019-11-14 16:05:36 +0100

    Updated detectTags

--- src/fhv_labyrinth/nodes/detectTags.py
+++ src/fhv_labyrinth/nodes/detectTags.py
@@ -2,25 +2,61 @@
 
 import rospy
 from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
 import cv2 as cv
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 1,
+    BLUE = 2
+
 
 class Detection():
+
+    current_pose = (0, 0, 0)
 
     def __init__(self):
         self.detectTags()
 
+    def setCurrentPose(self, data):
+
+        try:
+            self.current_pose = data.pose.pose.position
+        except:
+            rospy.logerr("AN ERROR OCCURED WHILE SETTING POSE")
+
     def processImage(self, data):
-        # rospy.loginfo(data.header)
+        COLOR = Color.RED
+
+        LOWER_LIMIT = UPPER_LIMIT = np.array([0, 0, 0])
+
+        if COLOR == Color.RED:
+            LOWER_LIMIT = np.array([0, 50, 50])
+            UPPER_LIMIT = np.array([10, 255, 255])
+        elif COLOR == Color.BLUE:
+            LOWER_LIMIT = np.array([30, 150, 50])
+            UPPER_LIMIT = np.array([255, 255, 180])
 
         bridge = CvBridge()
 
         try:
-            cv_image = bridge.imgmsg_to_cv2(data, "passthrough")
+            frame = bridge.imgmsg_to_cv2(data, "passthrough")
 
-            cv.imwrite('/home/markus/catkin_ws/test.jpeg', cv_image)
+            hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+            mask = cv.inRange(hsv, LOWER_LIMIT, UPPER_LIMIT)
+            res = cv.bitwise_and(frame, frame, mask=mask)
 
-            rospy.loginfo(cv_image)
+            if cv.mean(res) > (4, 4, 4):
+                rospy.loginfo("TAG FOUND @ x: {} y: {} z: {}".format(self.current_pose.x, self.current_pose.y, self.current_pose.z))
+
+                # TODO:
+                # publish to token-found-topic
+
+            cv.imshow('res', res)
+            cv.waitKey(1)
 
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
@@ -31,31 +67,13 @@ class Detection():
 
         try:
             while not rospy.is_shutdown():
-                # pub = rospy.Publisher('detectTags', TagDetected, queue_size=10)
-
-                # print("asdf")
-                # get camera image
-
-                sub_image = rospy.Subscriber(IMAGE_TOPIC, Image, self.processImage)
-
-                # rospy.loginfo(text)
-                # filter out blobs
-
-                # check if tag is in image
-
-                # tag = TagDetected()
-                #
-                # tag.test = 3
-                #
-                # pub.publish(tag)
+                rospy.Subscriber(IMAGE_TOPIC, Image, self.processImage)
+                rospy.Subscriber("odom", Odometry, self.setCurrentPose)
                 rospy.spin()
 
         finally:
-            rospy.loginfo('a')
-            # twist = Twist()
-            # twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-            # twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-            # pub.publish(twist)
+            rospy.loginfo('Shutting down node...')
+
 
 def main():
     rospy.init_node('detectTags')
