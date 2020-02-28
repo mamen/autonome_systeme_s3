27e26c1ca938d7ff0c88ad78e0a5eca60901514f
mamen <markusme@edu.aau.at>
2020-02-28 10:37:38 +0100

    Removed unused files

--- unterlagen/TurtlebotCode/initializeTurtleBlobs.sh
+++
@@ -1,3 +0,0 @@
-source ~/catkin_ws/devel/setup.bash 
-rosrun pixy_node pixy_node &
-roslaunch turtlebot3_bringup turtlebot3_robot.launch
--- unterlagen/TurtlebotCode/initializeTurtleCam.sh
+++
@@ -1,3 +0,0 @@
-source ~/catkin_ws/devel/setup.bash 
-rosrun beginner_camera pixy_cam &
-roslaunch turtlebot3_bringup turtlebot3_robot.launch
